b8d907df319b9f7f93aad16246d01e5570026060
gunny6026 <gunny6026@naver.com>
2021-01-14 21:30:11 +0900

    callback.js

---
+++ syntax/callback.js
@@ -0,0 +1,29 @@
+// function a(){
+//     console.log('A');
+// }
+
+// a라는 변수에 함수라는 값을 정의
+// 자바스크립트 에서는 함수가 값이다!!
+var a = function(){
+    console.log('A');
+}
+
+var b = function(){
+    console.log('B');
+}
+
+var cd = function(){
+    console.log('hello');
+}
+
+//오랜 시간이 걸리는 함수
+// 이 기능에 대한 실행이 끝난 다음에 
+// 함수가 실행이 끝났으니깐 다음 일을 하세요!!
+// 매개변수로 callback를 설정
+function slowfunc(ccccc){
+    ccccc();
+}
+
+slowfunc(a);  //  A
+slowfunc(b); //   B
+slowfunc(cd); // hello
